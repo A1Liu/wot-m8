4dd22dfce2b5df58eed03f8f73dbe65c97a82556
Albert Liu <albertymliu@gmail.com>
2022-04-17 18:37:40 -0400

    blarg

--- js/index.js
+++ js/index.js
@@ -1,15 +1,42 @@
-// define our imports
-var imports = {
-    imports: {
-    }
+const imports = {
+  imports: {},
 };
 
 fetch("binary.wasm")
-  .then((response) => { return response.arrayBuffer(); })
-  .then((bytes) => { return WebAssembly.instantiate(bytes, imports); })
-  .then((results)=> {
+  .then((response) => response.arrayBuffer())
+  .then((bytes) => WebAssembly.instantiate(bytes, imports))
+  .then((results) => {
     instance = results.instance;
     // grab our exported function from wasm
     const add = results.instance.exports.add;
     console.log(add(3, 4));
+  });
+
+const navbarInput = document.getElementById("navbarInput");
+const navbarUploadButton = document.getElementById("navbarUploadButton");
+
+const readFile = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+
+    reader.readAsText(file);
+  });
+
+navbarUploadButton.addEventListener("click", (evt) => {
+  evt.preventDefault();
+
+  navbarInput.click();
+});
+
+navbarInput.addEventListener("change", (evt) => {
+  evt.preventDefault();
+
+  // evt.target.files is a FileList
+  const files = Array.from(evt.target.files);
+
+  files.forEach((file) => {
+    console.log("changed", file);
+  });
 });
