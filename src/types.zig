7e8574ea190072a68ed3e4c1e6d98bb4f846f911
Albert Liu <albertymliu@gmail.com>
2022-04-20 23:02:55 -0400

    meh

--- src/types.zig
+++ src/types.zig
@@ -5,20 +5,42 @@ const debug = std.log.debug;
 const info = std.log.info;
 const err = std.log.err;
 
-const ArrayList = std.ArrayList;
-const HashMap = std.AutoArrayHashMap;
-
-const File = struct {
-    name: []const u8,
-    text: []const u8,
-};
+const ArrayList = std.ArrayListUnmanaged;
+const HashMap = std.AutoArrayHashMapUnmanaged;
 
 pub const FileDb = struct {
-    file_arenas: ArrayList([]u8),
-    files: ArrayList(File),
-    by_name: HashMap([]const u8, usize),
+    const Self = @This();
+
+    const File = struct {
+        name: []const u8,
+        text: []const u8,
+    };
+
+    arena: liu.Bump,
+    files: ArrayList(File), // uses arena, because it never really resizes
+    by_name: HashMap([]const u8, usize), // uses page allocator
+
+    pub fn init(size: usize) Self {
+        return Self{
+            .arena = liu.Bump.init(size, liu.Pages),
+            .files = ArrayList(File).init(),
+            .by_name = HashMap([]const u8, usize).init(),
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.by_name.deinit(self.arena.alloc);
+        self.arena.deinit();
+    }
+
+    pub fn addFile(self: *Self, name: []const u8, text: []const u8) u32 {
+        _ = self;
+        _ = name;
+        _ = text;
+    }
 };
 
+// uses page allocator
 pub const Symbols = struct {
     names: ArrayList([]const u8),
     to_symbol: HashMap([]const u8, usize),
