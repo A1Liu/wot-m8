5afa18da4a7b18b9ab810e3f2b75bd4a4043acdc
Albert Liu <albertymliu@gmail.com>
2022-04-24 03:41:14 -0400

    meh

--- js/windows.js
+++ js/windows.js
@@ -1,3 +1,22 @@
+const WINDOWS = "windows-panel";
+const HORIZONTAL = "windows-horizontal";
+const VERTICAL = "windows-vertical";
+
 const root = window.getElementById("windowRoot");
 
-export const verticalSplit = () => {};
+export const addHorizontalSibling = (domNode, child) => {
+  if (domNode.classList.contains(HORIZONTAL)) {
+    return;
+  }
+
+  if (domNode === root) {
+  }
+
+
+};
+
+
+export const verticalSplit = () => {
+};
+
+
